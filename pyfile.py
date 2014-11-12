58cf34cdd73a475e7dd31abb4a79964fdceb1c68
wienreb <m.wienreb@f5.com>
2014-11-12 16:19:40 +0200

    Files

---
+++ pyfile.py
@@ -0,0 +1,16 @@
+import sys
+import nose
+
+def printme(s):
+    s2 = s + '!'
+    return s2
+
+def test_printme():
+    assert printme('') == '!'
+
+def test_printme2():
+    assert printme('123') == '23!'
+
+if __name__ == '__main__':
+    # Watch out! Actual code may be coming down ...
+    printme('')
